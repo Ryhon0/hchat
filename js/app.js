5b9cd604e23cdc2df5e06d10ad4b183539ec13c9
Ryhon <ryhon@ryhn.link>
2024-04-26 23:31:20 +0200

    Support for annoucement messages

--- js/app.js
+++ js/app.js
@@ -702,6 +702,11 @@ function processMessage(pm, beforeElem = undefined) {
 						return;
 					}
 					break;
+				case "announcement":
+					mi.classList.add("announcement");
+					pm.content = pm.content ?? pm.tags["system-msg"];
+					debugger;
+					break;
 				default:
 					console.log("Unhandled USERNOTICE, msg-id: " + pm.tags["msg-id"]);
 					console.log(pm);
